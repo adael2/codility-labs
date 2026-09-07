fd2b7ad7817a70afe3952bd636ef40916e824df8
adita0898@gmail.com <adita0898@gmail.com>
2026-09-07 14:26:11 -0700

    Nesting

---
+++ Lessons/Lesson 7/nesting.py
@@ -0,0 +1,17 @@
+def solution(S):
+    # Implement your solution here
+    stack = []
+    partners = {')': '('}
+    for s in S:
+        if s == '(':
+            stack.append(s)
+        else:
+            if not stack:
+                return 0
+            partner = stack.pop()
+            if partner != partners[s]:
+                return 0
+    if not stack:
+        return 1
+    else:
+        return 0
